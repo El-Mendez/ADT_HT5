43b2ef264a7b1ac373fd8b35198c79affd248d20
Pablo Mendez <pablomendez778@gmail.com>
2020-03-04 18:59:54 -0600

    Terminado!! (la progra)

--- src/Lab5.py
+++ src/Lab5.py
@@ -7,16 +7,16 @@ import random
 
 # ============ Valores que cambian según cada simulación ============
 RAM_memoria = 100  # La cantidad de memoria en la RAM
-CPU_ips = 1  # Instrucciones por unidad de tiempo que lee el CPU
-time_given = 3  # Es la cantidad de tiempo que se le da al procesador como máximo para atender c/ proceso
-cantidad_procesos = 3  # La cantidad de procesos que entraran al CPU
-intervalos_procesos = 10  # El intervalo del tiempo en que llegan los procesos (ditr exponencial)
+CPU_ips = 3  # Instrucciones por unidad de tiempo que lee el CPU
+cantidad_procesos = 5  # La cantidad de procesos que entraran al CPU
+intervalos_procesos = 10  # El intervalo del tiempo en que llegan los procesos (distr exponencial)
 cantidad_CPU = 1  # Es la cantidad de CPUs en el sistema para atender los procesos
 
 random.seed(10)
 
 # =========================== Simulación ===========================
 run_time = []  # Lista con los tiempos totales de cada proceso
+time_given = 1  # Es la cantidad de tiempo que se le da al procesador como máximo para cada proceso
 
 
 def process(env, name, ram, cpu, cpu_ips, max_time, total_time, arrival_intervals):
@@ -26,15 +26,15 @@ def process(env, name, ram, cpu, cpu_ips, max_time, total_time, arrival_interval
     # Espero hasta que el carro aparezca
     yield env.timeout(random.expovariate(1 / arrival_intervals))
     time = env.now
-    print("El proceso %s ha sido creado en %.2f \t\t\t Instrucciones restantes: %d"
-          % (name, env.now, instructions))
+    print("\033[94mEl proceso %s ha sido creado en %.2f \t\t\t Cantidad de instrucciones: %d \tMemoria RAM requerida: %d \033[0m"
+          % (name, env.now, instructions, memory_needed))
 
     # Durante el ciclo completo necesito RAM
     yield ram.get(memory_needed)
 
     while instructions > 0:
-        print("El proceso %s llegó a la fase \"ready\" en %.2f \t\t Instrucciones restantes: %d"
-              %(name, env.now, instructions))
+        print("El proceso %s llegó a la fase \"ready\" en %.2f \t\t Instrucciones restantes: %d \t RAM restante: %d"
+              %(name, env.now, instructions, ram.level))
 
         # Mientras tengo instrucciones pido espacio al CPU
         yield cpu.get(1)
@@ -59,7 +59,7 @@ def process(env, name, ram, cpu, cpu_ips, max_time, total_time, arrival_interval
 
     # Al quedarme sin insrucciones imprimo que el proceso terminó y decuelvo la RAM
     yield ram.put(memory_needed)
-    print("El proceso %s fue finalizado en %.2f \t Instrucciones restantes: %d"
+    print("\033[92mEl proceso %s fue finalizado en %.2f \t Instrucciones restantes: %d\033[0m"
           % (name, env.now, instructions))
 
     # Añado el tiempo total a la lista total_time para guardar resultados
@@ -92,7 +92,10 @@ def desviacion(cantidades):
     for cantidad in cantidades:
         r = r + (media - cantidad) ** 2
 
-    return (r / (len(cantidades) - 1)) ** 0.5
+    if len(cantidades) > 1:
+        return (r / (len(cantidades) - 1)) ** 0.5
+    else:
+        return 0
 
 
 print("\nTiempos Totales:")
